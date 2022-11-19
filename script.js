6969d0c392cbf9be69569c010c6aa830a6f9f080
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2022-11-19 12:22:28 -0800

    added a bunch of typeof data checks

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-console.log("Practice makes perfect!");
+console.log("Assign values to variables and log them to the console:");
 
 var catName = "Gibbs";
 var catAge = 10;
 
 console.log(`${catName} is ${catAge} years old.`);
+
+console.log(`---------------------------------`);
+
+console.log(`Primitive datatypes checked using 'typeof' operator:`)
+
+var myUndefined;
+var myString = "Hello World";
+var myNumberInt = 69;
+var myNumberDecimal = 69.69;
+var myBooleanTrue = true;
+var myBooleanFalse = false;
+var myArray = [1,2,3,4,5];
+var myNull = null;
+var myZero = 0;
+var myDate = Date();
+var myNaN = NaN;
+
+console.log(typeof myUndefined);
+console.log(typeof myString);
+console.log(typeof myNumberInt);
+console.log(typeof myNumberDecimal);
+console.log(typeof myBooleanTrue);
+console.log(typeof myBooleanFalse);
+console.log(typeof myArray);
+console.log(typeof myNull);
+console.log(typeof myZero);
+console.log(typeof myDate);
+console.log(myDate);
+console.log(typeof myNaN);
+
+console.log(`---------------------------------`);
 
 
 
