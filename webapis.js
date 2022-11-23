181080a9f380c3ba17f75fdc5b9c2f71d8e7539c
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2022-11-23 07:09:56 -0800

    played around with traversing the DOM and setting attributes

---
+++ webapis.js
@@ -0,0 +1,50 @@
+// console.log(`----------------------------------`)
+// console.log(`WINDOW OBJECT`)
+// console.log(`----------------------------------`)
+
+// console.log(this);
+// console.log(window.document);
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+
+// console.log(`----------------------------------`)
+// console.log(`TRAVERSE THE DOM`)
+// console.log(`----------------------------------`)
+
+// console.log(document.body.children);
+// console.log(document.body.children[0]);
+// console.log(document.body.children[1]);
+// console.log(document.body.children[2]);
+// console.log(document.body.children[3]);
+// console.log(document.body.children[4]);
+// console.log(document.body.children[5]);
+// console.log(document.body.children[5].children[0]);
+
+// console.log(`----------------------------------`)
+// console.log(`SETTING ATTRIBUTES`)
+// console.log(`----------------------------------`)
+
+var headline = document.getElementById("headline");
+console.log(headline);
+
+headline.style.color = "#e97451";
+headline.style.fontSize = "50px";
+
+// var gibbsFavList = document.getElementById("gibbs-fav-list");
+// console.log(gibbsFavList);
+
+// gibbsFavList.children[1].style.color = "#e97451";
+
+var liTags = document.querySelectorAll("li");
+for (i = 0; i < liTags.length; i++) {
+    liTags[i].setAttribute("style", "color:blue; font-size:20px");
+}
+
+var gibbsFavTitle = document.querySelector("#gibbs-fav-title");
+gibbsFavTitle.setAttribute("style", "font-size: 30px; font-weight: bold; text-decoration: underline; color: red");
+
+gibbsFavTitle.textContent = "Gibbs Top List";
+
+var gibbsFriend = document.getElementById("gibbs-friend");
+gibbsFriend.setAttribute("src", "https://placekitten.com/g/200/200");
